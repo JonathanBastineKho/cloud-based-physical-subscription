ce23b93b65bdc79b0dfd183461fb190c4f4a05fa
Jonathan Bastine Kho <jonathanbastine@gmail.com>
2022-10-18 13:27:27 +0800

    Add register page

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -9,6 +9,42 @@ module.exports = {
   theme: {
     extend: {},
   },
+  fontFamily: {
+    'body': [
+  'Inter', 
+  'ui-sans-serif', 
+  'system-ui', 
+  '-apple-system', 
+  'system-ui', 
+  'Segoe UI', 
+  'Roboto', 
+  'Helvetica Neue', 
+  'Arial', 
+  'Noto Sans', 
+  'sans-serif', 
+  'Apple Color Emoji', 
+  'Segoe UI Emoji', 
+  'Segoe UI Symbol', 
+  'Noto Color Emoji'
+],
+    'sans': [
+  'Inter', 
+  'ui-sans-serif', 
+  'system-ui', 
+  '-apple-system', 
+  'system-ui', 
+  'Segoe UI', 
+  'Roboto', 
+  'Helvetica Neue', 
+  'Arial', 
+  'Noto Sans', 
+  'sans-serif', 
+  'Apple Color Emoji', 
+  'Segoe UI Emoji', 
+  'Segoe UI Symbol', 
+  'Noto Color Emoji'
+]
+  },
   plugins: [
     require("flowbite/plugin")
   ],
