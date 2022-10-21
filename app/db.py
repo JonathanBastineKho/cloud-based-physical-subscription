5d03e0f6601da15a5f5a064bbf9a8faf7d8f761d
Edbert Ekaputera <edbertekaputera@gmail.com>
2022-10-21 13:05:05 +0800

    update database structure

--- app/db.py
+++ app/db.py
@@ -1,15 +1,49 @@
-from app import datab
+from app import datab as db
 
-class User(datab.Model):
+class User(db.Model):
     __tablename__ = "user"
-    id = datab.Column(datab.integer, primary_key=True)
-    email = datab.Column(datab.String(250), nullable=False)
-    username = datab.Column(datab.String(250), nullable=False)
-    password = datab.Column(datab.String(1000), nullable=False)
+    # Attributes
+    username = db.Column("username", db.String(250))
+    email = db.Column("email", db.String(250), nullable=False)
+    password = db.Column("password", db.String(1000), nullable=False)
+    # Constraints
+    primary_key = db.PrimaryKeyConstraint("username", name="userPK")
+    candidate_key1 = db.UniqueConstraint("email", name="userCK1")
 
-class Company(datab.Model):
+class Company(db.Model):
     __tablename__ = "company"
-    id = datab.Column(datab.integer, primary_key=True)
-    email = datab.Column(datab.String(250), nullable=False)
-    username = datab.Column(datab.String(250), nullable=False)
-    password = datab.Column(datab.String(1000), nullable=False)
+    # Attributes
+    username = db.Column("username", db.String(250))
+    email = db.Column("email", db.String(250), nullable=False)
+    password = db.Column("password", db.String(1000), nullable=False)
+    company_name = db.Column("company_name", db.String(250), nullable=False)
+    address = db.Column("address", db.String(250), nullable=False)
+    # Constraints
+    primary_key = db.PrimaryKeyConstraint("username", name="companyPK")
+    candidate_key1 = db.UniqueConstraint("email", name="companyCK1")
+    candidate_key2 = db.UniqueConstraint("company_name", "address", name="companyCK2")
+
+class Door(db.Model):
+    __tablename__ = "door"
+    # Attributes
+    company = db.Column("company", db.String(250))
+    door_id = db.Column("door_id", db.Integer)
+    description = db.Column("description", db.String(1000), nullable=False)
+    # Constraints
+    primary_key = db.PrimaryKeyConstraint("company", "door_id", name="doorPK")
+    foreign_key1 = db.ForeignKeyConstraint(["company"], ["company.username"])
+    
+class Key(db.Model):
+    __tablename__ = "key"
+    # Attributes
+    company = db.Column("company", db.String(250))
+    door_id = db.Column("door_id", db.Integer)
+    user = db.Column("user", db.String(250))
+    start_date = db.Column("start_date", db.Date)
+    end_date = db.Column("end_date", db.Date)
+    duration = end_date - start_date # How to make computed column idk?
+    # Constraints
+    primary_key = db.PrimaryKeyConstraint("company", "door_id", "username", name="keyPK")
+    foreign_key1 = db.ForeignKeyConstraint(["company", "door_id"], ["door.company", "door.door_id"])
+    foreign_key2 = db.ForeignKeyConstraint(["user"], ["user.username"])
+    check1 = db.CheckConstraint("end_date > start_date", name="check1") # Not sure on this
--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os 
+from dotenv import load_dotenv
 
+load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-flask-key'
-    FLASK_APP = os.environ.get('FLASK_APP') or 'application.py'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SECRET_KEY = os.getenv('SECRET_KEY') or 'default-flask-key'
+	FLASK_APP = os.getenv('FLASK_APP') or 'application.py'
+	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	
+	@classmethod
+	def print(cls):
+		print(cls.SECRET_KEY, cls.SQLALCHEMY_DATABASE_URI, cls.FLASK_APP, cls.SQLALCHEMY_TRACK_MODIFICATIONS)
