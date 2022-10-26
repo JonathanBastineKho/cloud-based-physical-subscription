6138f73425ed23f248f56d244678e31502e8b477
Jonathan Bastine Kho <jonathanbastine@gmail.com>
2022-10-26 16:24:40 +0800

    Add login register functionality for company

--- app/db.py
+++ app/db.py
@@ -1,18 +1,44 @@
 from app import datab
+from flask_login import UserMixin
 
-class User(datab.Model):
+class User(UserMixin, datab.Model):
     __tablename__ = "user"
-    id = datab.Column(datab.Integer, primary_key=True)
-    email = datab.Column(datab.String(250), nullable=False)
-    username = datab.Column(datab.String(250), nullable=False)
+    username = datab.Column(datab.String(250), primary_key=True)
+    email = datab.Column(datab.String(250), nullable=False, unique=True)
     password = datab.Column(datab.String(1000), nullable=False)
+    key = datab.relationship("Key", back_populates="user")
+    
+    def get_id(self):
+        return self.username
 
-class Company(datab.Model):
+class Company(UserMixin, datab.Model):
     __tablename__ = "company"
-    id = datab.Column(datab.Integer, primary_key=True)
-    email = datab.Column(datab.String(250), nullable=False)
-    username = datab.Column(datab.String(250), nullable=False)
+    username = datab.Column(datab.String(250), primary_key=True)
+    email = datab.Column(datab.String(250), nullable=False, unique=True)
     password = datab.Column(datab.String(1000), nullable=False)
+    address = datab.Column(datab.String(250), nullable=False)
+    door = datab.relationship("Door", back_populates="company")
+
+    def get_id(self):
+        return self.username
+
+class Door(datab.Model):
+    __tablename__ = "door"
+    door_id = datab.Column(datab.Integer, primary_key=True)
+    description = datab.Column(datab.String(1000), nullable=False)
+    company = datab.relationship("Company", back_populates="door")
+    company_username = datab.Column(datab.Integer, datab.ForeignKey('company.username')) 
+    key = datab.relationship("Key", back_populates="door")
+
+class Key(datab.Model):
+    __tablename__ = "key"
+    company_username = datab.Column(datab.Integer, datab.ForeignKey('company.username'), primary_key=True)
+    door_id = datab.Column(datab.Integer, datab.ForeignKey('door.door_id'), primary_key=True)
+    door = datab.relationship("Door", back_populates="key")
+    user_username = datab.Column(datab.String(250), datab.ForeignKey('user.username'), primary_key=True)
+    user = datab.relationship("User", back_populates="key")
+    start_time = datab.Column(datab.Date, primary_key=True)
+    end_time = datab.Column(datab.Date, nullable=False)
+    duration = datab.column_property(start_time - end_time)
 
 datab.create_all()
-print("initialize")
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
 from flask import render_template, url_for, request, redirect
 from app import app, login_manager, bcrypt, datab
 from flask_login import login_user, login_required, current_user, logout_user
-from app.db import User
+from app.db import User, Company
+from flask import session
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
+def load_user(username):
+    user_type = session["user_type"]
+    if user_type == "individual":
+        return User.query.get(username)
+    elif user_type == "company":
+        return Company.query.get(username)
 
 @app.route('/')
 def index():
@@ -22,20 +27,55 @@ def register():
             password=bcrypt.generate_password_hash(request.form["password"], 13, prefix=b"2b"))
             datab.session.add(user)
             datab.session.commit()
+            session['user_type'] = 'individual'
             login_user(user)
             return redirect(url_for('index'))
     return render_template('register.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        user = User.query.filter_by(email=request.form["email"])
+        user = User.query.filter_by(email=request.form["email"]).first()
         if user != None and bcrypt.check_password_hash(pw_hash=user.password, password=request.form["password"]):
+            session['user_type'] = 'individual'
             login_user(user)
             return redirect(url_for('index'))
     return render_template('login.html')
+
+@app.route("/companylogin", methods=["GET", "POST"])
+def complogin():
+    if request.method == "POST":
+        company = Company.query.filter_by(email=request.form["email"]).first()
+        if company != None and bcrypt.check_password_hash(pw_hash=company.password, password=request.form["password"]):
+            session['user_type'] = 'company'
+            login_user(company)
+            return redirect(url_for('index'))
+    return render_template("complogin.html")
+
+@app.route("/companyregister", methods=["GET", "POST"])
+def compregister():
+    if request.method == "POST":
+        email = request.form["email"]
+        username = request.form["username"]
+        address = request.form["address"]
+        password = request.form["password"]
+        if Company.query.filter_by(email=email).first() == None and Company.query.filter_by(username=username).first() == None:
+            company = Company(username=username,
+            email=email,
+            password=bcrypt.generate_password_hash(password, 13, prefix=b"2b"),
+            address=address)
+            datab.session.add(company)
+            datab.session.commit()
+            session['user_type'] = 'company'
+            login_user(company)
+            return redirect(url_for('index'))
+    return render_template("compregister.html")
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
