8f87e3b933e95b4f82fdd67da534b4ec1b2ca696
Jonathan Bastine Kho <jonathanbastine@gmail.com>
2022-11-13 14:58:11 +0800

    Update index.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -22,39 +22,31 @@ async function openScanModal(){
         scanModal = new Modal(document.getElementById("scanModal"), openScanModal);
         scanModal.show();
     });
-    if (navigator.mediaDevices.getUserMedia){
-        navigator.mediaDevices.getUserMedia({video: true})
-        .then((res) => {
-            Html5Qrcode.getCameras().then(devices => {
-                if (devices && devices.length) {
-                    var cameraId = devices[0].id;
-                    // .. use this to start scanning.
-                    html5QrCode = new Html5Qrcode("scanQR");
-                    html5QrCode.start(
-                    cameraId, 
-                    {
-                        fps: 10   // Optional, frame per seconds for qr code scanning
-                        // qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
-                    },
-                    (decodedText, decodedResult) => {
-                        // do something when code is read
-                        console.log(decodedText);
-                    },
-                    (errorMessage) => {
-                        // parse error, ignore it.
-                    }).then(() => {
-                        $("#scanQR video").addClass("rounded-lg");
-                    })
-                    .catch((err) => {
-                    // Start failed, handle it.
-                    });
-                }
+    Html5Qrcode.getCameras().then(devices => {
+        if (devices && devices.length) {
+            var cameraId = devices[0].id;
+            // .. use this to start scanning.
+            html5QrCode = new Html5Qrcode("scanQR");
+            html5QrCode.start(
+            cameraId, 
+            {
+                fps: 10   // Optional, frame per seconds for qr code scanning
+                // qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
+            },
+            (decodedText, decodedResult) => {
+                // do something when code is read
+                console.log(decodedText);
+            },
+            (errorMessage) => {
+                // parse error, ignore it.
+            }).then(() => {
+                $("#scanQR video").addClass("rounded-lg");
             });
-        })
-        .catch((perm) => {
-            $("#scanQR").append("<p>permission denined</p>")
-        });
-    }
+        }
+    })
+    .catch((err) => {
+        $("#scanQR").append("<p>permission denined</p>");
+    });
 }
 
 function closeScanModal(){
