a09aa8a1f3e1323eba2b3e9031450fbe1ef04b62
Carlos <c.frasenda@yahoo.com>
2022-11-16 21:27:03 +0800

    Simulation stuff
    door pics + html for door simulation + js for door simulation

---
+++ app/static/js/simulation.js
@@ -0,0 +1,30 @@
+var gStatus = document.getElementById('status').innerHTML;
+function checkDoor(status)
+{
+    var image = document.getElementById("doorIMG");
+    status=String(status);
+    gStatus=status
+    if (status == "Open")
+    {
+        image.src="../static/img/openDoor.png";
+        image.alt="Door Opened";
+    }
+    else if (status == "Close")
+    {
+        image.src="../static/img/closeDoor.png";
+        image.alt="Door Closed";
+    }
+    else
+    {
+        image.src="";
+        image.alt="Door Not Found";
+    }
+}
+
+window.setInterval('refresh()', 5000); 
+function refresh() {
+    window .location.reload();
+}
+
+console.log(gStatus);
+checkDoor(gStatus);
