5cfe2dc85c6566bd271db0ed41befcad77ea9055
Edbert Ekaputera <edbertekaputera@gmail.com>
2022-11-19 17:08:50 +0800

    update key

--- app/static/js/key.js
+++ app/static/js/key.js
@@ -70,8 +70,16 @@ async function cancelSubscription(formData) {
 	.then(function(res){
 		return res.json();
 	}).then(function(data){
+		if (data.status) {
+			$("#subscription-cancel-alert_icon > svg").empty();
+			$("#subscription-cancel-alert_icon > svg").append(
+				$('<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>')
+			);
+		}
 		$("#subscription-cancel-alert_text").text(data.message);
 		$("#subscription-cancel-alert").removeClass("hidden");
+		sleep(3000);
+		hideToast();
 	})
 }
 
@@ -84,4 +92,8 @@ function cancelBtn(event){
 function hideToast(){
 	$("#subscription-cancel-alert").addClass("hidden");
 	window.location.reload();
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
